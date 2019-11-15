ef703c006824df5cb8e4fe3b2bbd3d524d37c0fb
the-sides <sides798@gmail.com>
2019-11-15 13:36:33 -0500

    more readable and output file for decryption

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-python3.7 ./rsa-keygen -p key.pub -s key -n 4096
+python3.7 ./rsa-keygen -p key.pub -s key -n 2048 > /dev/null
 echo '\n++++++++++++++++++++++++++++++++'
 echo 'public key (key.pub) contents'
 echo '++++++++++++++++++++++++++++++++'
@@ -7,7 +7,15 @@ echo '\n++++++++++++++++++++++++++++++++'
 echo 'private key (key) contents'
 echo '++++++++++++++++++++++++++++++++'
 cat key
-python3.7 rsa-enc -k key.pub -i msg -o output
-python3.7 rsa-dec -k key -i output -o washed
+echo '\n++++++++++++++++++++++++++++++++'
+echo 'Encrypting message...'
+echo '++++++++++++++++++++++++++++++++'
+cat msg
+python3.7 rsa-enc -k key.pub -i msg -o output > /dev/null
+echo '\n++++++++++++++++++++++++++++++++'
+echo 'Decrypted message (washed)...'
+echo '++++++++++++++++++++++++++++++++'
+python3.7 rsa-dec -k key -i output -o washed > /dev/null
+cat washed
 echo '\ndeleting key...'
 # rm -rf key.pub key
